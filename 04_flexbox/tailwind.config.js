a59fabfd94a79a2c2e750ec0c9d2acc1106f7c37
Usiel Lopes <usiel.lopes@sinteseconsultores.com.br>
2023-07-19 20:01:26 -0300

    Padronizando as Aspas

--- 04_flexbox/tailwind.config.js
+++ 04_flexbox/tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
